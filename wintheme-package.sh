cf0a863b50be19b37fd8d9b6fdf71d2a8cf50dc2
tPenguinLTG <tpenguinltg@hotmail.com>
2015-08-12 20:41:53 -0400

    Add licence header to file

--- wintheme-package.sh
+++ wintheme-package.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 WINTHEME_PREVIEW_PATH="${WINTHEME_PREVIEW_PATH:-previews}"
 WINTHEME_RAW_PATH="${WINTHEME_RAW_PATH:-raw}"
 WINTHEME_BRANDING_FILE="$(realpath ${WINTHEME_BRANDING_FILE:-branding.png})"
